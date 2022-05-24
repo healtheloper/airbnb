ecb7f1a39ce4499097efd7453566b0d990447011
healtheloper <58503584+healtheloper@users.noreply.github.com>
2022-05-24 16:41:07 +0900

    feat: color data

--- src/constants/color.js
+++ src/constants/color.js
@@ -1,3 +1,14 @@
-const color = {};
+const color = {
+  white: '#fff',
+  grey1: '#333',
+  grey2: '#4F4F4F',
+  grey3: '#828282',
+  grey4: '#BDBDBD',
+  grey5: '#E0E0E0',
+  grey6: '#F5F5F7',
+  black: '#010101',
+  primary: '#E84C60',
+  secondary: '#118917',
+};
 
 export default color;
