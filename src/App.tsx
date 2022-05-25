3295a35b229fa32232ebe87fd2431e2568b20cb0
Muffin <jinlog9@gmail.com>
2022-05-25 12:22:18 +0900

    style: theme provider 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,22 @@
+import { ThemeProvider } from '@mui/material/styles';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+import { theme } from '@common/theme';
 import Layout from '@components/Layout';
-
-import MainPage from './pages/MainPage';
+import MainPage from '@pages/MainPage';
 
 export default function App() {
   return (
     <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<MainPage />} />
-          </Route>
-        </Routes>
-      </Router>
+      <ThemeProvider theme={theme}>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route index element={<MainPage />} />
+            </Route>
+          </Routes>
+        </Router>
+      </ThemeProvider>
     </div>
   );
 }
