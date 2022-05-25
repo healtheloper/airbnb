2db1148c247ebd484bc6014880a7f745ff0fa95d
Sangjin Park <58503584+healtheloper@users.noreply.github.com>
2022-05-25 16:46:55 +0900

    [Park] user info fab click -> menu items
    * feat: user info fab click -> menu items
    
    * style: px -> rem

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,31 @@
+import { GlobalStyles } from '@mui/material';
+import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider } from '@mui/material/styles';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import { theme } from '@common/theme';
 import Layout from '@components/Layout';
+import color from '@constants/color';
 import MainPage from '@pages/MainPage';
+
+function MyGlobalStyles() {
+  return (
+    <GlobalStyles
+      styles={{
+        body: {
+          backgroundColor: color.bgColor,
+          color: color.grey1,
+        },
+      }}
+    />
+  );
+}
 
 export default function App() {
   return (
     <div className="App">
+      <CssBaseline />
+      <MyGlobalStyles />
       <ThemeProvider theme={theme}>
         <Router>
           <Routes>
