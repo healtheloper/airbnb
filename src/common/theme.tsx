8b99bb0f1ba9af775bbdca1f50ed2c7933323d0b
Muffin <jinlog9@gmail.com>
2022-05-26 15:04:56 +0900

    feat: 타입을 정의하는 declare module 부분을 types 폴더로 따로 분리 하여 사용 할 수 있도록 작업

--- src/common/theme.tsx
+++ src/common/theme.tsx
@@ -1,29 +1,7 @@
 import { createTheme } from '@mui/material/styles';
-import React from 'react';
 
 import color from '@constants/color';
 import fontSize from '@constants/fontSize';
-
-declare module '@mui/material/styles' {
-  interface TypographyVariants {
-    input1: React.CSSProperties;
-    input2: React.CSSProperties;
-  }
-
-  // allow configuration using `createTheme`
-  interface TypographyVariantsOptions {
-    input1?: React.CSSProperties;
-    input2?: React.CSSProperties;
-  }
-}
-
-// Update the Typography's variant prop options
-declare module '@mui/material/Typography' {
-  interface TypographyPropsVariantOverrides {
-    input1: true;
-    input2: true;
-  }
-}
 
 export const theme = createTheme({
   palette: {
---
+++ src/types/theme.d.ts
@@ -0,0 +1,22 @@
+import React from 'react';
+
+declare module '@mui/material/styles' {
+  interface TypographyVariants {
+    input1: React.CSSProperties;
+    input2: React.CSSProperties;
+  }
+
+  // allow configuration using `createTheme`
+  interface TypographyVariantsOptions {
+    input1?: React.CSSProperties;
+    input2?: React.CSSProperties;
+  }
+}
+
+// Update the Typography's variant prop options
+declare module '@mui/material/Typography' {
+  interface TypographyPropsVariantOverrides {
+    input1: true;
+    input2: true;
+  }
+}
