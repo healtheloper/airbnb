82917e6fd1dbe3174b22fb9a885b7e0a7a81d393
Muffin <jinlog9@gmail.com>
2022-06-02 14:21:54 +0900

    feat: 린트 강제 적용되는 부분 삭제, 네이밍 수정, 주석 추가

--- src/components/Chart/index.tsx
+++ src/components/Chart/index.tsx
@@ -7,9 +7,19 @@ import FlexBox from '@components/FlexBox';
 import color from '@constants/color';
 import rooms from '@mocks/room';
 
-const CANVAS_WIDTH = 400;
+/**
+ * 전역변수
+ * CANVAS_WIDTH : 캔버스 가로 크기
+ * CANVAS_HEIGHT : 캔버스 세로 크기
+ * normalDistributionValue : 차트를 그릴때 기준이되는 x축 값
+ * sliderInterval : 슬라이더가 완전히 겹쳐지지 않도록 각 슬라이더에 +-해주는 값
+ * percentage: 캔버스의 높이와 최대값으로 백분율을 위해 곱해주는 100
+ */
+const CANVAS_WIDTH = 365;
 const CANVAS_HEIGHT = 100;
 const normalDistributionValue = 100000;
+const sliderInterval = 10000;
+const percentage = 100;
 
 interface cavansDataProps {
   [key: number]: number;
@@ -26,10 +36,12 @@ interface roomsProps {
 
 type AirbnbThumbComponentProps = React.HTMLAttributes<unknown>;
 
-function AirbnbThumbComponent(props: AirbnbThumbComponentProps) {
-  const { children, ...other } = props;
+// 슬라이더 내 || 를 표시하기위해 컴포넌트 확장
+function AirbnbThumbComponent({
+  children,
+  ...other
+}: AirbnbThumbComponentProps) {
   return (
-    // eslint-disable-next-line react/jsx-props-no-spreading
     <SliderThumb {...other}>
       {children}
       <span className="airbnb-bar" />
@@ -65,59 +77,83 @@ const draw = (
   const ctx = canvas?.getContext('2d');
 
   if (ctx) {
+    // 최소값 이전의 값들만 뽑아내기
     const minPriceObj = Object.keys(priceObj)
       .filter(price => +price <= min)
       .reduce((obj: cavansDataProps, key: any) => {
-        // eslint-disable-next-line no-param-reassign
         obj[key] = priceObj[key];
         return obj;
       }, {});
 
+    // 최대값 이후의 값들만 뽑아내기
     const maxPriceObj = Object.keys(priceObj)
       .filter(price => +price >= max)
       .reduce((obj: cavansDataProps, key: any) => {
-        // eslint-disable-next-line no-param-reassign
         obj[key] = priceObj[key];
         return obj;
       }, {});
 
+    // 최소 ~ 최대 값들만 뽑아내기
     const betweenPriceObj = Object.keys(priceObj)
       .filter(price => +price >= min && +price <= max)
       .reduce((obj: cavansDataProps, key: any) => {
-        // eslint-disable-next-line no-param-reassign
         obj[key] = priceObj[key];
         return obj;
       }, {});
 
+    /**
+     * @param prevData: 이전 숙소 값
+     * @param xIndex: x축의 위치를 상대적으로 나타내는 값으로 xInterval의 위치를 동적으로 변경하기 위한 값 (600000원 이하 값들만 들어오다면 현재 100000원 기준으로 그려주고 있기 때문에 idx는 0 ~ 7까지 들어가게됨)
+     * @param prev?: x또는 y축 이전 값을 저장하는 값
+     */
     let prevData = 0;
-    let idx = 0;
+    let xIndex = 0;
     let prevX = 0;
     let prevY = 0;
     let x = 0;
     let y = 0;
 
+    /**
+     *
+     * @param obj: {300000: 7} 형태로 가격: 숙소 개수 형태로 들어있음.
+     * @param xInterval : x축 위치
+     * @param maxRooms : 숙소 개수 최대값
+     * bezierCurveTo(첫 번째 제어점 x, 첫 번째 제어점 y, 두 번째 제어점 x, 두 번째 제어점y, 끝 점의 x좌표, 끝 점의 y좌표)
+     * 기준 점(첫 번째 제어점)을 하나 정하고 → 곡선의 형태를 지정하는 또 두개의 제어점(두 번째 제어점) →  곡선을 끝낼 기준점(끝 점)
+     */
     const drawBezierCurve = (
       obj: cavansDataProps,
       xInterval: number,
-      maxPrice: number,
+      maxRooms: number,
     ) => {
       Object.values(obj).forEach((data: number) => {
-        x = xInterval * idx;
-        prevX = xInterval * (idx - 1);
-        y = CANVAS_HEIGHT - (data / maxPrice) * 100;
-        prevY = CANVAS_HEIGHT - (prevData / maxPrice) * 100;
+        x = xInterval * xIndex;
+        prevX = xInterval * (xIndex - 1);
+        y = CANVAS_HEIGHT - (data / maxRooms) * percentage;
+        prevY = CANVAS_HEIGHT - (prevData / maxRooms) * percentage;
         const controlPoint = (x + prevX) / 2;
         ctx.bezierCurveTo(controlPoint, prevY, controlPoint, y, x, y);
         prevData = data;
-        idx += 1;
+        xIndex += 1;
       });
     };
 
-    const fillArea = (xPoint: number, yPoint: number, areaColor: string) => {
-      ctx.strokeStyle = areaColor;
-      ctx.fillStyle = areaColor;
-      ctx.stroke();
+    const priceObjLength = Object.keys(priceObj).length;
+    const xInterval = Math.floor(CANVAS_WIDTH / priceObjLength);
+    const maxRooms = Math.max(...Object.values(priceObj));
+
+    /**
+     *
+     * @param xPoint: 색상을 채우고 채워진 영역 x끝점에서 다시 그려주기 시작하기 위한 값
+     * @param yPoint: 색상을 채우고 채워진 영역 y끝점에서 다시 그려주기 시작하기 위한 값
+     * @param fillColor: 해당 영역을 채울 색상
+     *
+     */
+    const fillArea = (xPoint: number, yPoint: number, fillColor: string) => {
       ctx.lineTo(xPoint, CANVAS_HEIGHT);
+      ctx.strokeStyle = color.bgColor;
+      ctx.fillStyle = fillColor;
+      ctx.stroke();
       ctx.fill();
       ctx.closePath();
       ctx.beginPath();
@@ -125,21 +161,17 @@ const draw = (
       ctx.lineTo(xPoint, yPoint);
     };
 
-    const priceObjLength = Object.keys(priceObj).length;
-    const xInterval = Math.floor(CANVAS_WIDTH / priceObjLength);
-    const maxPrice = Math.max(...Object.values(priceObj));
-
     ctx.moveTo(0, CANVAS_HEIGHT);
     // min 값 이전 그리기
-    drawBezierCurve(minPriceObj, xInterval, maxPrice);
+    drawBezierCurve(minPriceObj, xInterval, maxRooms);
     fillArea(x, y, color.bgColor);
 
     // min ~ max 값 그리기
-    drawBezierCurve(betweenPriceObj, xInterval, maxPrice);
+    drawBezierCurve(betweenPriceObj, xInterval, maxRooms);
     fillArea(x, y, color.black);
 
     // max 값 이후 그리기
-    drawBezierCurve(maxPriceObj, xInterval, maxPrice);
+    drawBezierCurve(maxPriceObj, xInterval, maxRooms);
 
     ctx.strokeStyle = color.bgColor;
     ctx.stroke();
@@ -171,10 +203,8 @@ const calculateRangeCount = () => {
         standard += normalDistributionValue;
       }
 
-      // eslint-disable-next-line no-param-reassign
       if (prev[standard] === undefined) prev[standard] = 0;
 
-      // eslint-disable-next-line no-param-reassign
       prev[standard] += 1;
       return prev;
     },
@@ -207,29 +237,29 @@ export default function Chart() {
 
   const handleSlider = (
     event: Event,
-    newValue: number | number[],
+    sliderNewValue: number | number[],
     activeThumb: number,
   ) => {
-    if (!Array.isArray(newValue)) {
+    if (!Array.isArray(sliderNewValue)) {
       return;
     }
 
     if (activeThumb === 0) {
       setSliderValue([
-        Math.min(newValue[0], sliderValue[1] - 10000),
+        Math.min(sliderNewValue[0], sliderValue[1] - sliderInterval),
         sliderValue[1],
       ]);
     } else {
       setSliderValue([
         sliderValue[0],
-        Math.max(newValue[1], sliderValue[0] + 10000),
+        Math.max(sliderNewValue[1], sliderValue[0] + sliderInterval),
       ]);
     }
 
     draw(
       canvasRef.current,
-      sliderValue[0],
-      sliderValue[1],
+      sliderNewValue[0],
+      sliderNewValue[1],
       calculateRangeCount(),
     );
   };
@@ -256,6 +286,7 @@ export default function Chart() {
         height: '22.75rem',
         padding: '3.25rem 4rem 4.8rem',
         backgroundColor: color.white,
+        borderRadius: '2.5rem',
       }}
       jc="center"
       fd="column"
