bb3dbf9a9c1dae52e0e7a85d735295c3184d1a2c
healtheloper <58503584+healtheloper@users.noreply.github.com>
2022-06-07 17:06:56 +0900

    feat: render by persons data

--- src/components/Persons/index.tsx
+++ src/components/Persons/index.tsx
@@ -2,6 +2,7 @@ import { AddCircleOutline, RemoveCircleOutline } from '@mui/icons-material';
 import { Box, Typography, List, ListItem, IconButton } from '@mui/material';
 
 import FlexBox from '@components/FlexBox';
+import { PersonStringType, usePersonState } from '@contexts/PersonProvider';
 
 const listStyle = {
   display: 'flex',
@@ -16,54 +17,61 @@ const listItemStyle = {
   justifyContent: 'space-between',
 };
 
+type InfoType = {
+  title: string;
+  desc: string;
+};
+
+type PersonInfoType = {
+  [key in PersonStringType]: InfoType;
+};
+
+const personInfo: PersonInfoType = {
+  adult: {
+    title: '성인',
+    desc: '만 13세 이상',
+  },
+  child: {
+    title: '어린이',
+    desc: '만 2~12세',
+  },
+  baby: {
+    title: '유아',
+    desc: '만 2세 미만',
+  },
+};
+
+const renderPersonItem = (person: [string, number]) => {
+  const [personType, personCount] = person;
+  const type = personType as PersonStringType;
+  const { title, desc }: InfoType = personInfo[type];
+  const isLastElement = personType === 'baby';
+
+  return (
+    <ListItem key={personType} sx={listItemStyle} divider={!isLastElement}>
+      <Box>
+        <Typography variant="h5">{title}</Typography>
+        <Typography variant="input2">{desc}</Typography>
+      </Box>
+      <FlexBox ai="center" sx={{ gap: '0.5rem' }}>
+        <IconButton aria-label="한명 줄이기">
+          <RemoveCircleOutline />
+        </IconButton>
+        <Typography variant="h5">{personCount}</Typography>
+        <IconButton aria-label="한명 추가하기">
+          <AddCircleOutline />
+        </IconButton>
+      </FlexBox>
+    </ListItem>
+  );
+};
+
 export default function Persons() {
+  const personState = usePersonState();
+  const persons: [string, number][] = Object.entries(personState);
   return (
     <List sx={listStyle}>
-      <ListItem sx={listItemStyle} divider>
-        <Box>
-          <Typography variant="h5">성인</Typography>
-          <Typography variant="input2">만 13세 이상</Typography>
-        </Box>
-        <FlexBox ai="center" sx={{ gap: '0.5rem' }}>
-          <IconButton aria-label="한명 줄이기">
-            <RemoveCircleOutline />
-          </IconButton>
-          <Typography variant="h5">0</Typography>
-          <IconButton aria-label="한명 추가하기">
-            <AddCircleOutline />
-          </IconButton>
-        </FlexBox>
-      </ListItem>
-      <ListItem sx={listItemStyle} divider>
-        <Box>
-          <Typography variant="h5">어린이</Typography>
-          <Typography variant="input2">만 2~12세</Typography>
-        </Box>
-        <FlexBox ai="center" sx={{ gap: '0.5rem' }}>
-          <IconButton aria-label="한명 줄이기">
-            <RemoveCircleOutline />
-          </IconButton>
-          <Typography variant="h5">0</Typography>
-          <IconButton aria-label="한명 추가하기">
-            <AddCircleOutline />
-          </IconButton>
-        </FlexBox>
-      </ListItem>
-      <ListItem sx={listItemStyle}>
-        <Box>
-          <Typography variant="h5">유아</Typography>
-          <Typography variant="input2">만 2세 미만</Typography>
-        </Box>
-        <FlexBox ai="center" sx={{ gap: '0.5rem' }}>
-          <IconButton aria-label="한명 줄이기">
-            <RemoveCircleOutline />
-          </IconButton>
-          <Typography variant="h5">0</Typography>
-          <IconButton aria-label="한명 추가하기">
-            <AddCircleOutline />
-          </IconButton>
-        </FlexBox>
-      </ListItem>
+      {persons.map(person => renderPersonItem(person))}
     </List>
   );
 }
