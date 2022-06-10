a16e69ff17f666d0ba2265baff23f6703f89785b
healtheloper <58503584+healtheloper@users.noreply.github.com>
2022-06-10 10:19:00 +0900

    fix: .env github client id add

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ const webpack = require('webpack');
 dotenv.config();
 
 const mode = process.env.NODE_ENV || 'development';
-const { KAKAO_MAP_KEY, NODE_ENV } = process.env;
+const { KAKAO_MAP_KEY, NODE_ENV, GITHUB_CLIENT_ID } = process.env;
 
 const PORT = 3000;
 
@@ -61,6 +61,7 @@ module.exports = {
     new webpack.EnvironmentPlugin({
       KAKAO_MAP_KEY,
       NODE_ENV,
+      GITHUB_CLIENT_ID,
     }),
   ],
   stats: 'errors-only',
